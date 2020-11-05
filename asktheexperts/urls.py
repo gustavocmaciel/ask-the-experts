650758716f38f3bce67217c1983cccc4f534a15e
gcmaciel <gustavocmaciel1993@gmail.com>
2020-11-05 17:09:55 -0300

    Add register page

--- asktheexperts/urls.py
+++ asktheexperts/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #if settings.DEBUG:
