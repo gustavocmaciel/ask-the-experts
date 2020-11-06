2941fc08d3352f548804bf2d61f8b3d726abaa83
gcmaciel <gustavocmaciel1993@gmail.com>
2020-11-06 14:48:26 -0300

    Add field rank to user model

--- asktheexperts/models.py
+++ asktheexperts/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     #photo = models.ImageField(default="/media/default_image.jpg",)
-    #rank = models.IntegerField(default=1)
     
     #photo = models.FileField(upload_to='images/', null=True, verbose_name="")
+    vote_question = models.ManyToManyField("Question", symmetrical=False, related_name="question_voted")
+    vote_answer = models.ManyToManyField("Answer", symmetrical=False, related_name="answer_voted")
+    rank = models.IntegerField(default=1)
 
-    pass
+    def __str__(self):
+        return f"{self.id} - {self.username}"
+
 
 class Question(models.Model):
     user = models.ForeignKey("User", on_delete=models.PROTECT, related_name="questioned")
@@ -25,17 +29,7 @@ class Answer(models.Model):
     question = models.ForeignKey("Question", on_delete=models.PROTECT, related_name="answered_question")
     content = models.TextField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
-    answered = models.BooleanField(default=False)
+    selected = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.id} - {self.user} answered {self.question}"
-
-
-class Votes(models.Model):
-    user = models.ForeignKey("User", on_delete=models.PROTECT, related_name="voted_user")
-    question = models.ForeignKey("Question", on_delete=models.ProtectedError, related_name="question_voted")
-    number_of_votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.id} - {self.user} voted {self.question}"
-    
