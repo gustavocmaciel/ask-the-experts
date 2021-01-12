5c9cc8a7fae15046d816f85e8836428fb8782368
gcmaciel <66797203+gcmaciel@users.noreply.github.com>
2021-01-12 16:12:24 -0300

    Set environment variable

--- capstone/settings.py
+++ capstone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2=@9hhb8rsp%uv+i)#(+azpt^djldl0c*8d-w9oqis#2zq)i)r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
